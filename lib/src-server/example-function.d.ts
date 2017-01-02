1264cb3543088c730e8a2806fc0a628766fc27f0
Rick Love <rick@toldpro.com>
2017-01-02 16:56:00 -0600

    FIX tsc ignore  lib

---
+++ lib/src-server/example-function.d.ts
@@ -0,0 +1,3 @@
+import * as T from "../src/types";
+import { ExampleFunctionRequest, ExampleFunctionResponseData } from "../src/example-function-model";
+export declare function main(context: T.Context<ExampleFunctionResponseData>, request: ExampleFunctionRequest): void;
