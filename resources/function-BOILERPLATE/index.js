7eddc5d3c7884e77a80148fd731ee2dda3b03ed1
Rick Love <rick@toldpro.com>
2017-01-03 16:47:38 -0600

    FIX Boilerplate path

--- resources/function-BOILERPLATE/index.js
+++ resources/function-BOILERPLATE/index.js
@@ -1,1 +1,1 @@
-module.exports = require('./../lib/src-server/azure-server').serve(require('./../lib/src-server/FUNCTION_NAME').main);
+module.exports = require('@told/azure-functions-server/lib/src-server/azure-server').serve(require('./../lib/src-server/FUNCTION_NAME').main);
